0dc11fbdb404ac88ea253dd27d9b620cdf58c5cf
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-09 20:57:08 +0200

    feat: add a mirror plane under the scene

--- src/Experience/models/decoration/index.tsx
+++ src/Experience/models/decoration/index.tsx
@@ -1,5 +1,6 @@
 import Grass from './Grass'
 import LightPole from './LightPole'
+import Mirror from './Mirror'
 import Pavement from './Pavement'
 import Tree from './Tree'
 
@@ -10,6 +11,7 @@ const Decoration = () => {
       <LightPole />
       <Tree />
       <Grass />
+      <Mirror />
     </group>
   )
 }
