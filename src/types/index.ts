05e2b60651da8c54ba0edfbbee86f78cd45b31fb
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-14 14:40:09 +0200

    feat: add Destination & camera state types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,11 @@
 export type HoverMessage = string
+
+export interface Destination {
+  to: THREE.Vector3
+  focusPoint: THREE.Vector3
+}
+
+export interface CameraState {
+  focusPoint: THREE.Vector3
+  position: THREE.Vector3
+}
