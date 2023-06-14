1d233761a075e14e0771e9456ef29f1f7f4ef0e7
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-14 14:37:59 +0200

    fix: change button to html div to fix pointer

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,11 +1,15 @@
 import './style.css'
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   text: string
 }
 
-const Button: React.FC<Props> = ({ text }) => {
-  return <button className="closeButton">{text}</button>
+const Button: React.FC<Props> = ({ text, ...props }) => {
+  return (
+    <div {...props} className="closeButton">
+      {text}
+    </div>
+  )
 }
 
 export default Button
