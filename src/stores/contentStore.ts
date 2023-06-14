9482541bbe6581bcddd8df3a3bb6a41e713f895d
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-14 14:39:48 +0200

    feat: add currentState to transition store & focused content to content store

--- src/stores/contentStore.ts
+++ src/stores/contentStore.ts
@@ -4,14 +4,30 @@ interface ContentState {
   content:
     | { isContentVisible: true; contentId: string }
     | { isContentVisible: false; contentId: undefined }
+  focusedContent:
+    | { isContentVisible: true; contentId: string }
+    | { isContentVisible: false; contentId: undefined }
   selectContent: (contentId: string) => void
   clearContent: () => void
+  focusContent: (contentId: string) => void
+  exitFocusedContent: () => void
 }
 
 export const useContentStore = create<ContentState>()((set) => ({
   content: { isContentVisible: false, contentId: undefined },
   selectContent: (contentId: string) =>
-    set(() => ({ content: { isContentVisible: true, contentId: contentId } })),
+    set(() => ({ content: { isContentVisible: true, contentId } })),
   clearContent: () =>
     set(() => ({ content: { isContentVisible: false, contentId: undefined } })),
+
+  focusedContent: { isContentVisible: false, contentId: undefined },
+  focusContent: (contentId: string) => {
+    set(() => ({
+      focusedContent: { isContentVisible: true, contentId },
+    }))
+  },
+  exitFocusedContent: () =>
+    set(() => ({
+      focusedContent: { isContentVisible: false, contentId: undefined },
+    })),
 }))
--- src/stores/transitionStore.ts
+++ src/stores/transitionStore.ts
@@ -1,17 +1,36 @@
 import { create } from 'zustand'
-import { Destination } from '../types'
+import { CameraState, Destination } from '../types'
+import { sceneCenter, sceneCenterCameraPosition } from '../config'
 
 interface TransitionState {
   isTransitionActive: boolean
   destination?: Destination
+  currentState: CameraState
   startTransition: (to: Destination) => void
   endTransition: () => void
 }
 
 export const useTransitionStore = create<TransitionState>()((set) => ({
   isTransitionActive: false,
+  destination: undefined,
+  currentState: {
+    focusPoint: sceneCenter,
+    position: sceneCenterCameraPosition,
+  },
   startTransition: (to: Destination) =>
     set(() => ({ isTransitionActive: true, destination: to })),
   endTransition: () =>
-    set(() => ({ isTransitionActive: false, destination: undefined })),
+    set((state) => {
+      if (!state.destination)
+        throw new Error('Cannot end transition without end destination')
+      const { focusPoint, to } = state.destination
+      return {
+        isTransitionActive: false,
+        destination: undefined,
+        currentState: {
+          focusPoint: focusPoint,
+          position: to,
+        },
+      }
+    }),
 }))
