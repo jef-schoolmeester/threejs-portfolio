7e98b3d783aa2fdcf2badefb92d03a140adcc3b0
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-14 14:48:54 +0200

    feat: add perf, useTransition, focusManager & content manager to scene

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience/index'
 import Cursor from './Components/Cursor'
-import ContentOverlay from './Components/ContentOverlay'
+import ContentManager from './Components/ContentManager'
+import { cameraStartPosition } from './config'
+import FocusManager from './Components/FocusManager'
 
 const App = () => {
   return (
@@ -10,7 +12,7 @@ const App = () => {
       <Canvas
         camera={{
           fov: 50,
-          position: [2, 5, -20],
+          position: cameraStartPosition,
         }}
         gl={{ outputColorSpace: 'srgb' }}
       >
@@ -21,6 +23,8 @@ const App = () => {
         title="Summary"
         content="As a full stack developer, I thrive on the challenges of building robust and dynamic web applications. With a solid understanding of databases and server-side technologies like SQL and PHP, I am adept at designing and optimizing back-end systems. I also have a strong grasp of front-end frameworks such as Vue.js and Bootstrap, allowing me to create responsive and visually appealing user interfaces. I am passionate about writing clean, maintainable code and staying up-to-date with the latest industry trends. My focus is on delivering high-quality solutions that meet client requirements and provide an exceptional user experience."
       /> */}
+      <ContentManager />
+      <FocusManager />
     </>
   )
 }
--- src/Experience/index.tsx
+++ src/Experience/index.tsx
@@ -1,14 +1,18 @@
-import { Environment, MeshReflectorMaterial } from '@react-three/drei'
+import { Environment } from '@react-three/drei'
 import Models from './models'
 import Helpers from './Helpers'
 import Camera from './camera'
 import { useInitialTransition } from './hooks/useInitialTransition'
 import Effects from './Effects'
+import { Perf } from 'r3f-perf'
+import { useAnimatedTransition } from './hooks/useAnimatedTransition'
 
 const Experience = () => {
   useInitialTransition()
+  useAnimatedTransition()
   return (
     <>
+      <Perf position="top-left" />
       <Models />
       <Camera />
       <Environment preset="forest" />
