7242d2aeb6126b46daba463986392cde1ea68b69
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-14 14:38:29 +0200

    feat: create config file for key positions

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import * as THREE from 'three'
+
+export const cameraStartPosition = new THREE.Vector3(2, 5, -20)
+export const cameraStartFocusPoint = new THREE.Vector3(0, 2.5, -1)
+
+export const sceneCenter = new THREE.Vector3(0, 2.5, -1)
+export const sceneCenterCameraPosition = new THREE.Vector3(10, 1.8, 2)
+
+export const columnFocusPoint = new THREE.Vector3()
+export const columnCameraPosition = new THREE.Vector3()
