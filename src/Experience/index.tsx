f635e634935214e340a02c6ff39ee66b348bce5d
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-21 18:43:46 +0200

    fix: move hooks out of experience folder imports

--- src/Experience/index.tsx
+++ src/Experience/index.tsx
@@ -2,10 +2,10 @@ import { Environment } from '@react-three/drei'
 import Models from './models'
 import Helpers from './Helpers'
 import Camera from './camera'
-import { useInitialTransition } from './hooks/useInitialTransition'
+import { useInitialTransition } from '../hooks/useInitialTransition'
 import Effects from './Effects'
 import { Perf } from 'r3f-perf'
-import { useAnimatedTransition } from './hooks/useAnimatedTransition'
+import { useAnimatedTransition } from '../hooks/useAnimatedTransition'
 
 const Experience = () => {
   useInitialTransition()
