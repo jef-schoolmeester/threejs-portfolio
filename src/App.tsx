317bf6f121b19c4f1a3e1eb0c155dfb7c54f191a
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-21 18:46:45 +0200

    feat: remove unused comment in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,6 @@ const App = () => {
         <Experience />
       </Canvas>
       <Cursor />
-      {/* <ContentOverlay
-        title="Summary"
-        content="As a full stack developer, I thrive on the challenges of building robust and dynamic web applications. With a solid understanding of databases and server-side technologies like SQL and PHP, I am adept at designing and optimizing back-end systems. I also have a strong grasp of front-end frameworks such as Vue.js and Bootstrap, allowing me to create responsive and visually appealing user interfaces. I am passionate about writing clean, maintainable code and staying up-to-date with the latest industry trends. My focus is on delivering high-quality solutions that meet client requirements and provide an exceptional user experience."
-      /> */}
       <ContentManager />
       <FocusManager />
       <KioskContentManager />
