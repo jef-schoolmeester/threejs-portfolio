e62bdc7f216131fdc31112af7d134c4d17dea83d
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-26 10:58:47 +0200

    feat: add loading screen variable to content store & update initial transition accordingly

--- src/hooks/useInitialTransition.ts
+++ src/hooks/useInitialTransition.ts
@@ -1,15 +1,21 @@
 import { useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useState } from 'react'
 import { sceneCenter, sceneCenterCameraPosition } from '../config'
+import { useContentStore } from '../stores/contentStore'
 
 export const useInitialTransition = () => {
+  const isLoadingScreenVisible = useContentStore(
+    (state) => state.isLoadingScreenVisible
+  )
+
   const state = useThree()
   const [shouldTransition, setShouldTransition] = useState<boolean>(false)
 
   useEffect(() => {
+    if (isLoadingScreenVisible) return
     state.camera.lookAt(sceneCenter)
-    setTimeout(() => setShouldTransition(true), 500)
-  }, [])
+    setShouldTransition(true)
+  }, [isLoadingScreenVisible])
 
   useFrame((state, delta) => {
     if (shouldTransition) {
--- src/stores/contentStore.ts
+++ src/stores/contentStore.ts
@@ -1,19 +1,26 @@
 import { create } from 'zustand'
 
 interface ContentState {
+  isLoadingScreenVisible: boolean
+  enterSite: () => void
+
   content:
-    | { isContentVisible: true; contentId: string }
-    | { isContentVisible: false; contentId: undefined }
-  focusedContent:
     | { isContentVisible: true; contentId: string }
     | { isContentVisible: false; contentId: undefined }
   selectContent: (contentId: string) => void
   clearContent: () => void
+
+  focusedContent:
+    | { isContentVisible: true; contentId: string }
+    | { isContentVisible: false; contentId: undefined }
   focusContent: (contentId: string) => void
   exitFocusedContent: () => void
 }
 
 export const useContentStore = create<ContentState>()((set) => ({
+  isLoadingScreenVisible: true,
+  enterSite: () => set(() => ({ isLoadingScreenVisible: false })),
+
   content: { isContentVisible: false, contentId: undefined },
   selectContent: (contentId: string) =>
     set(() => ({ content: { isContentVisible: true, contentId } })),
