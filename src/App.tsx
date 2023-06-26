eed98c39952de35cb9126432904cc002d7cf8470
jef schoolmeester <jef.schoolmeester99@gmail.com>
2023-06-26 14:18:14 +0200

    feat: put experience in suspence & add loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,42 @@ import ContentManager from './Components/ContentManager'
 import { cameraStartPosition } from './config'
 import FocusManager from './Components/FocusManager'
 import KioskContentManager from './Components/KioskContentManager/Manager'
+import { Suspense, useEffect, useState } from 'react'
+import LoadingScreen from './Components/LoadingScreen'
+import { useDebugMode } from './hooks/useDebugMode'
 
 const App = () => {
+  const [renderAllowed, setRenderAllowed] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRenderAllowed(true)
+    }, 500)
+  }, [])
+
+  const isDebugModeEnabled = useDebugMode()
+
   return (
     <>
       <Canvas
+        className="experience"
         camera={{
           fov: 50,
           position: cameraStartPosition,
         }}
         gl={{ outputColorSpace: 'srgb' }}
       >
-        <Experience />
+        {renderAllowed && (
+          <Suspense>
+            <Experience />
+          </Suspense>
+        )}
       </Canvas>
       <Cursor />
       <ContentManager />
       <FocusManager />
       <KioskContentManager />
+      {!isDebugModeEnabled && <LoadingScreen />}
     </>
   )
 }
